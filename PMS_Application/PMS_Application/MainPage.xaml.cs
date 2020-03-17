2a98c002e8d1c30b83af5f4500c7396ea7a30cce
LokeshKoshti <lokeshkumar.koshti@IFOUR-19>
2020-03-17 17:30:08 +0530

    initial commit

---
+++ PMS_Application/PMS_Application/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PMS_Application
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
